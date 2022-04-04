6c0188788617a1896334f7fef45c7ea815fcb900
Willie Klein <willie@royal-01.cs.wisc.edu>
2022-04-04 17:21:00 -0500

    Notes from TA about clone()

--- proc.c
+++ proc.c
@@ -227,26 +227,35 @@ int clone(void(*fcn)(void *, void *), void *arg1, void *arg2, void *stack)
 {
   int i, pid;
   struct proc *np;
-  struct proc *curproc = myproc();
+  struct proc *curproc = myproc(); // need this still
 
   // Allocate process.
   if((np = allocproc()) == 0){
     return -1;
   }
 
-  // Copy process state from proc.
-  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
-    kfree(np->kstack);
-    np->kstack = 0;
-    np->state = UNUSED;
-    return -1;
-  }
+  // DONT NEED THIS
+  // if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+  //   kfree(np->kstack);
+  //   np->kstack = 0;
+  //   np->state = UNUSED;
+  //   return -1;
+  // }
+
+  //eip, esp, edp are the registers we have to change. Need to look at the x86 file to see how.
+  // set eip pointer = *fcn
+  // have to manually move the stack pointer and know the order how the stack grows, like arg, ..., return. offseting by page size and such.
+
+  // This file has a decent amount of changes. But join() created from wait() only has like 3
+
+  //p_thread create and wait in the next step is basically just a wrapper for clone and join
+
   np->sz = curproc->sz;
   np->parent = curproc;
   *np->tf = *curproc->tf;
 
   // Clear %eax so that fork returns 0 in the child.
-  np->tf->eax = 0;
+  np->tf->eax = 0; //change register
 
   for(i = 0; i < NOFILE; i++)
     if(curproc->ofile[i])
